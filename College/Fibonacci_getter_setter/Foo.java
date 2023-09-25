f83ad1625ec3c3d0e134ff495dd1ae1ff00b4142
Ranbeer <ranbeermalhotra2004@gmail.com>
2023-09-25 18:40:43 +0530

    modified:   College/Fibonacci_getter_setter/Foo.java 	new file:   modifiers/modify.java

--- College/Fibonacci_getter_setter/Foo.java
+++ College/Fibonacci_getter_setter/Foo.java
@@ -3,15 +3,30 @@ import java.util.Scanner;
 class Foo {
     private int n; // private data member
 
-    public int recur_fibo(int n) {
-        this.n = n;
+    public void fibo() {
+        int a1 = 0, a2 = 1;
+        // no terms asked
         if (n == 0) {
-            return 0;
+            System.out.println("No terms to print out");
         }
+        // only 1 term asked
         else if (n == 1) {
-            return 1;
+            System.out.println(a1);
         }
-        return (recur_fibo(n - 1) + recur_fibo(n - 2));
+        // more than 1 terms asked
+        else {
+            System.out.printf("%d %d", a1, a2);
+            // two terms already printed therefore loop till n-2
+            for (int i = 0; i < (n - 2); i++)
+            {
+                int a3 = a1 + a2;
+                System.out.printf(" %d", a3);
+                a1 = a2;
+                a2 = a3;
+            }
+            // newline because printf does not
+            System.out.println();
+        }
     }
 
     // public getters and setters
@@ -19,10 +34,7 @@ class Foo {
         return this.n;
     }
 
-    public void setLen() {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Number of terms: ");
-        int len = input.nextInt();
+    public void setLen(int len) {
         this.n = len;
     }
 }
@@ -30,15 +42,13 @@ class Foo {
 class Bar {
 
     public static void main(String args[]) {
-        
+        Scanner input = new Scanner(System.in);
         Foo series = new Foo();
-        series.setLen();
-        for (int i = 0, len = series.getLen(); i < len; i++)
-        {   
-            // fibonacci using recursion 
-            System.out.printf("%d ", series.recur_fibo(i));
-        }
-        System.out.println(series.getLen());
-        System.out.println();
+        System.out.printf("Enter the no of term: ");
+        int nlen = input.nextInt();
+        series.setLen(nlen);
+        series.fibo();
+        int length = series.getLen();
+        System.out.println(length);
     }
 }
---
+++ modifiers/modify.java
@@ -0,0 +1,11 @@
+public class modify {
+    static void helloworld() {
+        System.out.println("Hello world");
+    }
+}
+
+class Second {
+    public static void main(String args[]) {
+        modify.helloworld();    
+    }
+}
